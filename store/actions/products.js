17f2749b2a3a940bcaba09e216c30ce6f7fd7cd3
Maksym Shokin <maksym.sh@upplabs.com>
2020-02-02 15:01:47 +0200

    'update-and-delete-product'

--- store/actions/products.js
+++ store/actions/products.js
@@ -6,7 +6,15 @@ export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const SET_PRODUCT = 'SET_PRODUCT';
 
 export const deleteProduct = productId => {
-  return { type: DELETE_PRODUCT, pid: productId };
+  return async dispatch => {
+    await fetch(`https://rn-shop-app-8fd49.firebaseio.com/products/${productId}.json`, {
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_PRODUCT, pid: productId
+    });
+  }
 };
 
 export const fetchProduct = () => {
@@ -38,7 +46,7 @@ export const fetchProduct = () => {
 
       }
 
-      dispatch({ type: SET_PRODUCT, products: loadedProducts });
+      dispatch({type: SET_PRODUCT, products: loadedProducts});
     } catch (error) {
       throw error
     }
@@ -62,7 +70,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
     const responseData = await response.json();
 
-    dispatch ({
+    dispatch({
       type: CREATE_PRODUCT,
       productData: {
         id: responseData.name,
@@ -76,13 +84,27 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    }
+  return async dispatch => {
+    await fetch(`https://rn-shop-app-8fd49.firebaseio.com/products/${id}.json`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl
+      })
+    });
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      }
+    })
   };
 };
