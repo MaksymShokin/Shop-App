7150e541eb37f251c9d002ff80349a3f67ff9d8b
Maksym Shokin <maksym.sh@upplabs.com>
2020-02-06 10:58:55 +0200

    'login-errors'

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -3,41 +3,8 @@ export const LOGIN = 'LOGIN';
 
 export const signup = (email, password) => {
   return async dispatch => {
-    console.log(email, password)
-    try {
-      const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDJPN4yui_yUKJ6DcSVvdyF37gz4FX8kuA',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-            returnSecureToken: true
-          })
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Something went wrong!')
-      }
-
-      const resData = await response.json();
-      console.log(resData)
-
-      dispatch({
-        type: SIGNUP
-      })
-    } catch (error) {
-      throw error
-    }
-  }
-};
-
-export const login = (email, password) => {
-  return async dispatch => {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDJPN4yui_yUKJ6DcSVvdyF37gz4FX8kuA',
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDJPN4yui_yUKJ6DcSVvdyF37gz4FX8kuA',
       {
         method: 'POST',
         headers: {
@@ -48,17 +15,56 @@ export const login = (email, password) => {
           password: password,
           returnSecureToken: true
         })
-
       }
     );
 
     if (!response.ok) {
-      throw new Error('Something went wrong!')
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = 'Something went wrong!';
+      if (errorId === 'EMAIL_EXISTS') {
+        message = 'This email exists already!';
+      }
+      throw new Error(message);
     }
 
     const resData = await response.json();
-    console.log(resData)
+    console.log(resData);
+    dispatch({ type: SIGNUP });
+  };
+};
 
-    dispatch({type: LOGIN})
-  }
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDJPN4yui_yUKJ6DcSVvdyF37gz4FX8kuA',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      const errorResData = await response.json();
+      const errorId = errorResData.error.message;
+      let message = 'Something went wrong!';
+      if (errorId === 'EMAIL_NOT_FOUND') {
+        message = 'This email could not be found!';
+      } else if (errorId === 'INVALID_PASSWORD') {
+        message = 'This password is not valid!';
+      }
+      throw new Error(message);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: LOGIN });
+  };
 };
